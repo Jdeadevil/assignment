6a9004fa752b5ede4965d633c620bbc7ebdb43cb
Jdeadevil <Jonnyd456@hotmail.com>
2016-03-25 20:38:02 +0000

    Update scripts.php

--- scripts.php
+++ scripts.php
@@ -4,9 +4,13 @@
 		<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
 		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
 		
-		<!-- jQuery Bootstrap scripts -->
+		<!-- jQuery / Bootstrap / custom scripts -->
+		<script src="js/jquery-1.11.1.min.js"></script>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
 
 		<!-- Loading main css file -->
 		<link rel="stylesheet" href="style.css">
